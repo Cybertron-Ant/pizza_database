2a2f9f32afaca71c7ef9941223d734bdeaccd352
Cybertron-Ant <Mediainformationofficer@gmail.com>
2021-09-15 11:35:05 -0500

    applied form validation to email, title, and ingredients form fields.

--- add.php
+++ add.php
@@ -1,12 +1,55 @@
-
 <?php
 
+/*
+// used this initially to check if form was working
 if (isset($_POST['submit'])) {
-  echo $_POST['email'];
-  echo $_POST['title'];
-  echo $_POST['ingredients'];
+//convert dangerous characters into html entities
+  echo htmlspecialchars($_POST['email']);
+  echo htmlspecialchars($_POST['title']);
+  echo htmlspecialchars($_POST['ingredients']); 
+
+} */
+
+
+//check email
+if (empty($_POST['email'])) {
+  echo "Your email is required <br />";
+} else {
+  $email = $_POST['email'];
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Please enter a vaild email address";
+  }
+}
+
+
+//check title
+if (empty($_POST['title'])) {
+  echo "Your pizza title is required <br />";
+} else {
+  
+  $tite = $_POST['title'];
+  //match lowercass and uppercase letters and a space
+    if (!preg_match('/^[a-zA-Z\s]+$/', $title )) {
+      echo "Title must be letters and spaces only. <br />";
+    }
+  }
+
+
+
+//check ingredients
+if (empty($_POST['ingredients'])) {
+  echo "At least one ingredient is required <br />";
+} else {
+  
+  $ingredients = $_POST['ingredient'];
+   //match comma separated words
+      if (!preg_match('/^([a-zA-Z\s])+(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+        echo "Ingredients must be a comma separated list. <br />";
+      }
 
 }
+
+
 
 ?>
 
