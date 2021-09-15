29adae4ade1e572c8f68866667b9ffdd754f228d
Cybertron-Ant <Mediainformationofficer@gmail.com>
2021-09-15 10:30:22 -0500

    header.php contains nav-bar includes bootstrap and materialize libraries

---
+++ templates/header.php
@@ -0,0 +1,39 @@
+
+
+   <head>
+     <title>My page</title>
+      <link rel = "stylesheet" href = "./node_modules/bootstrap.min.css">
+<link rel = "stylesheet" href = "./node_modules/materialize.min.css">
+
+
+
+<style type = "text/css">
+  .brand {
+    /*override materialize css classes */
+    background: cbb09c  !important;
+  }
+  /* hide-on-small-and-down  */
+  .brand-text {
+    /* text color */
+    color: cbb09c !important;
+  }
+
+  form {
+    max-width: 460px;
+    margin: 20px auto;
+    padding 20px;
+  }
+</style>
+
+
+</head>
+   <body class = "grey lighten-4">
+     <nav class = " z-depth-0">
+       <div class = "container">
+         <a href = "#" class = "brand-logo brand-text">Antonio's Pizza Depo</a>
+         <ul id = "nav-mobile" class = "right ">
+           <li><a href = "#" class = "btn brand z-depth-0">Add Pizza</a></li>
+         </ul>
+       </div>
+
+     </nav>
