85ab3ee033f6842beacd656d565cb6d522f412e9
Cybertron-Ant <Mediainformationofficer@gmail.com>
2021-09-16 13:37:38 -0500

    output soup title and email on details page

--- details.php
+++ details.php
@@ -1,4 +1,27 @@
 <?php 
+include('configurations/database_connection.php');
+
+// check GET request id parameter
+if(isset($_GET['id'])) {
+  
+  $id = mysqli_real_escape_string($conn, $_GET['id']);
+  
+  //create query
+  $sql = "SELECT * FROM soups WHERE id = $id";
+  
+  //run query
+  $result = mysqli_query($conn, $sql);
+  
+  //get result in associative array format
+  $mySoup = mysqli_fetch_assoc($result);
+  
+  mysqli_free_result($result);
+  mysqli_close($conn);
+  
+  
+  
+  
+}
 
 
 ?>
@@ -10,6 +33,17 @@
 <?php  include "templates/header.php";   ?>
 
 <h2>Soup Details</h2>
+<div class = "container center">
+  
+   <?php if($mySoup): ?>
+    <h4><?php echo htmlspecialchars($mySoup['title']); ?></h4>
+    <p>Created by: <?php echo htmlspecialchars($mySoup['email']); ?></p>
+
+   <?php else: ?>
+   
+   <?php endif; ?>
+
+</div>
 
 
 <?php  include "templates/footer.php";   ?>
