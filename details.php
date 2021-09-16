ab388a822d4044b6f43d96cb3d2d9d8d23b2fca8
Cybertron-Ant <Mediainformationofficer@gmail.com>
2021-09-16 15:19:24 -0500

    closed input tag and added comments for readability

--- details.php
+++ details.php
@@ -1,19 +1,20 @@
-<?php 
+<?php
 include('configurations/database_connection.php');
 
 if (isset($_POST['delete'])) {
-  
-  //reference hidden input form field 
+
+  //reference hidden input form field
+  //takes out malicious user input that may hurt database
   $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
-  
+
   $sql = "DELETE FROM soups WHERE id = $id_to_delete";
-  
+
   if (mysqli_query($conn, $sql)) {
     //if query is successful
     //redirect user to home page
-  
+
     //header('Location: index php');
-    
+
     //Or redirect user using some JavaScript
       echo "<script>
            //Using setTimeout to execute a function after 1 second.
@@ -21,36 +22,36 @@ if (isset($_POST['delete'])) {
               //Redirect with JavaScript
               window.location.assign('index.php');
            }, 1000);
-           </script>";  
-    
-    
+           </script>";
+
+
   } else {
     //if query failed
     echo "Error ". mysqli_query($conn);
   }
-  
+
 }
 
 // check GET request id parameter
 if(isset($_GET['id'])) {
-  
+
   $id = mysqli_real_escape_string($conn, $_GET['id']);
-  
+
   //create query
   $sql = "SELECT * FROM soups WHERE id = $id";
-  
+
   //run query
   $result = mysqli_query($conn, $sql);
-  
+
   //get result in associative array format
   $mySoup = mysqli_fetch_assoc($result);
-  
+
   mysqli_free_result($result);
   mysqli_close($conn);
-  
-  
-  
-  
+
+
+
+
 }
 
 
@@ -64,7 +65,7 @@ if(isset($_GET['id'])) {
 
 <h2 class="center">Soup Details</h2>
 <div class = "container center">
-  
+
    <?php if($mySoup): ?>
     <h4><?php echo htmlspecialchars($mySoup['title']); ?></h4>
     <p>Created by: <?php echo htmlspecialchars($mySoup['email']); ?></p>
@@ -75,15 +76,16 @@ if(isset($_GET['id'])) {
 <!-- DELETE functionality -->
 
 <form action = "details.php" method = "POST">
-  <input type = "hidden" name = "id_to_delete" value = "<?php echo $mySoup['id']  ?>"
+  <input type = "hidden" name = "id_to_delete" value = "<?php echo $mySoup['id']  ?>">
+<!-- has the value of the id that I want to delete -->
   <input type = "submit" name = "delete" value = "DELETE" class = "btn brand z-depth-0">
-  
+
 </form>
 
    <?php else: ?>
-   
+
    <h5>No such soup exists!</h5>
-   
+
    <?php endif; ?>
 
 </div>
