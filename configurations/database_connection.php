ab28d36dc197d0f009fa8c1eb7967b5a078e1b41
Cybertron-Ant <Mediainformationofficer@gmail.com>
2021-09-17 08:06:53 -0500

    edited database credentials

--- configurations/database_connection.php
+++ configurations/database_connection.php
@@ -1,4 +1,5 @@
-<?php 
+
+<?php /*
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
@@ -10,5 +11,23 @@ $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE
 //check database connection if it's true or false
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to database: ' . mysqli_connect_error());
-}
+}*/
  ?>
+ 
+ 
+
+ 
+ <?php 
+ $DATABASE_HOST = 'sql311.unaux.com';
+ $DATABASE_USER = 'unaux_29764247';
+ $DATABASE_PASS = 'Conglomeration1';
+ $DATABASE_NAME = 'unaux_29764247_soup_database';
+ 
+ //connect to database
+ $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+ 
+ //check database connection if it's true or false
+ if (mysqli_connect_errno()) {
+ 	exit('Failed to connect to database: ' . mysqli_connect_error());
+ }
+  ?>
